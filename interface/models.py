331c80ade593eb7deadc7d07eae0d07680f81eb6
Sean McIntyre <boxysean@gmail.com>
2012-03-16 15:53:36 -0400

    added models

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Emailer(models.Model):
+	name = models.CharField(max_length=200)
+	netId = models.CharField(max_length=200)
+	image = models.CharField(max_length=200)
+
+class EmailAddress(models.Model):
+	emailer = models.ForeignKey(Emailer)
+	emailAddress = models.CharField(max_length=200)
+
+class Team(models.Model):
+	name = models.CharField(max_length=200)
+	manager = models.ForeignKey(Emailer)
+
+class Player(models.Model):
+	team = models.ForeignKey(Team)
+	player = models.ForeignKey(Emailer)
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+class EmailerPointRanking(models.Model):
+	emailer = models.ForeignKey(Emailer)
+	category = models.ForeignKey(Category)
+	type = models.CharField(max_length=200)
+	points = models.IntegerField()
+
+class TeamPointRanking(models.Model):
+	team = models.ForeignKey(Team)
+	category = models.ForeignKey(Category)
+	type = models.CharField(max_length=200)
+	points = models.IntegerField()
+
