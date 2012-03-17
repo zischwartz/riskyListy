f58fdc676088db186a30f02b8ac4b1034a3d336f
Sean McIntyre <boxysean@gmail.com>
2012-03-17 13:30:42 -0400

    updated models and added data test bed

--- interface/admin.py
+++ interface/admin.py
@@ -1,11 +1,65 @@
 import models
 from django.contrib import admin
 
-admin.site.register(models.Emailer)
-admin.site.register(models.EmailAddress)
-admin.site.register(models.Team)
-admin.site.register(models.Player)
-admin.site.register(models.Category)
-admin.site.register(models.EmailerPointRanking)
-admin.site.register(models.TeamPointRanking)
+class EmailerAdmin(admin.ModelAdmin):
+	list_display = ("name",)
+	list_display_links = ("name",)
+	ordering = ("name",)
+
+admin.site.register(models.Emailer, EmailerAdmin)
+
+
+
+class EmailAddressAdmin(admin.ModelAdmin):
+	list_display = ("emailer", "emailAddress")
+	ordering = ("emailer",)
+	search_fields = ("emailer", "emailAddress")
+
+admin.site.register(models.EmailAddress, EmailAddressAdmin)
+
+
+
+class TeamAdmin(admin.ModelAdmin):
+	list_display = ("manager", "name")
+	ordering = ("manager",)
+	search_fields = ("manager",)
+
+admin.site.register(models.Team, TeamAdmin)
+
+
+
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ("player", "team", "points")
+	list_filter = ("team",)
+	ordering = ("team", "player")
+	search = ("team", "player")
+
+admin.site.register(models.Player, PlayerAdmin)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ("name",)
+
+admin.site.register(models.Category, CategoryAdmin)
+
+
+
+class EmailerPointRankingAdmin(admin.ModelAdmin):
+	list_display = ("emailer", "category", "type", "points")
+	list_filter = ("type",)
+	ordering = ("emailer", "category", "type")
+	search = ("emailer", "category", "type")
+
+admin.site.register(models.EmailerPointRanking, EmailerPointRankingAdmin)
+
+
+
+class TeamPointRankingAdmin(admin.ModelAdmin):
+	list_display = ("team", "category", "type", "points")
+	list_filter = ("type",)
+	ordering = ("team", "category", "type")
+	search = ("team", "category", "type")
+
+admin.site.register(models.TeamPointRanking, TeamPointRankingAdmin)
 
